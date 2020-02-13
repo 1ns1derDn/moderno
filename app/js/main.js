f1ad8bad3d27e56f2a8a2fc26b6d0189b0af118a
1ns1derDn <sergienko-98@mail.ua>
2020-02-13 13:12:57 +0200

    added section products-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
+
+  $('.products-slider__inner').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+
+  $('.rate-stars').rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
   var mixer = mixitup('.products__inner');
 });
