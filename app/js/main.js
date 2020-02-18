6ef183105cfdfa40689e745c676061bf27761019
1ns1derDn <sergienko-98@mail.ua>
2020-02-18 12:26:09 +0200

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(function(){
 
+  $('.icon-th-list').on('click', function(){
+    $('.products__item-list').addClass('list');
+    $('.icon-th-large').removeClass('active')
+    $('.icon-th-list').addClass('active')
+  });
+
+  $('.icon-th-large').on('click', function(){
+    $('.products__item-list').removeClass('list');
+    $('.icon-th-large').addClass('active')
+    $('.icon-th-list').removeClass('active')
+  });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+});
+
   $('.products-slider__inner').slick({
     arrows: false,
     dots: true,
